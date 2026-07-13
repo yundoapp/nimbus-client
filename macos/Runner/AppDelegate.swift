c354251852e2069f2ba09dfdbab0eabf938e5f44
wintion <wintion@gmail.com>
2026-07-13 17:48:57 +0800

    fix: 点击 Dock 图标恢复主窗口

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -7,21 +7,24 @@ class AppDelegate: FlutterAppDelegate {
     // https://github.com/leanflutter/window_manager/issues/214
     return false
   }
-  
+
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
-  // // window manager restore from dock: https://leanflutter.dev/blog/click-dock-icon-to-restore-after-closing-the-window
-  // override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
-  //     if !flag {
-  //         for window in NSApp.windows {
-  //             if !window.isVisible {
-  //                 window.setIsVisible(true)
-  //             }
-  //             window.makeKeyAndOrderFront(self)
-  //             NSApp.activate(ignoringOtherApps: true)
-  //         }
-  //     }
-  //     return true
-  // }
+
+  override func applicationShouldHandleReopen(
+    _ sender: NSApplication,
+    hasVisibleWindows flag: Bool
+  ) -> Bool {
+    guard let window = sender.windows.first(where: { $0 is MainFlutterWindow }) else {
+      return true
+    }
+
+    if window.isMiniaturized {
+      window.deminiaturize(nil)
+    }
+    window.makeKeyAndOrderFront(nil)
+    sender.activate(ignoringOtherApps: true)
+    return true
+  }
 }
